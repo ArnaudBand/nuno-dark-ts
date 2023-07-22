ed2edc235e6241dfbcd5f08a049f30784010b797
ArnaudBand <bandonkeyea@gmail.com>
2023-07-22 03:01:08 +0300

    Set the theme and check the preference of the user

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { ChangeEventHandler } from "react";
 import './Theme.css';
+
+// set theme based on user preference
+const setDarkMode = () => {
+  localStorage.setItem("theme", "dark");
+  document.documentElement.setAttribute("data-theme", "dark");
+}
+
+const setLightMode = () => {
+  localStorage.setItem("theme", "light");
+  document.documentElement.setAttribute("data-theme", "light");
+}
+
+// check if user has a preference stored
+const storedTheme = localStorage.getItem("theme");
+
+// check if user has a preference set in their OS
+const preferDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// set theme based on user preference
+const defaultDark = storedTheme === "dark" || (storedTheme === null && preferDark);
+
+// set theme based on user preference
+if (defaultDark) {
+  setDarkMode();
+} else {  
+  setLightMode();
+}
+
+// toggle theme based on user preference
+const toggleTheme: ChangeEventHandler<HTMLInputElement> = (e) => {
+  e.target.checked ? setDarkMode() : setLightMode();
+}
 
 
 const Theme = () => {
@@ -10,6 +42,8 @@ const Theme = () => {
         <input
           type="checkbox"
           id="checkbox"
+          onChange={toggleTheme}
+          defaultChecked={defaultDark}
         />
         <div className="slider round"></div>
       </label>
